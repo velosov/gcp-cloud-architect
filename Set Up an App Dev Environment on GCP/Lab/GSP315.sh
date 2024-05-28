097af85cfde2ddc956b7fd80e839953ccffe62c9
velosov <viniciusveloso@poli.ufrj.br>
2024-05-27 21:26:43 -0300

    Development Environments Challenge Lab

---
+++ Set Up an App Dev Environment on GCP/Lab/GSP315.sh
@@ -0,0 +1,146 @@
+REGION=us-central1
+ZONE=us-central1-c
+
+gcloud config set functions/region $REGION
+echo "1. Creating bucket"
+BCKT=qwiklabs-gcp-03-cf0978cf1bbc-bucket
+
+gsutil mb -l $REGION gs://$BCKT
+
+echo "2. Creating Pub/Sub topic"
+TPC=topic-memories-651
+gcloud pubsub topics create $TPC
+
+echo "3. Creating thumbnail Cloud Function"
+FUNC=memories-thumbnail-maker
+gcloud config set compute/region $REGION
+mkdir $FUNC && cd $FUNC
+
+cat << EOF > index.js
+const functions = require('@google-cloud/functions-framework');
+const crc32 = require("fast-crc32c");
+const { Storage } = require('@google-cloud/storage');
+const gcs = new Storage();
+const { PubSub } = require('@google-cloud/pubsub');
+const imagemagick = require("imagemagick-stream");
+
+functions.cloudEvent('memories-thumbnail-maker', cloudEvent => {
+  const event = cloudEvent.data;
+
+  console.log(`Event: ${event}`);
+  console.log(`Hello ${event.bucket}`);
+
+  const fileName = event.name;
+  const bucketName = event.bucket;
+  const size = "64x64"
+  const bucket = gcs.bucket(bucketName);
+  const topicName = "";
+  const pubsub = new PubSub();
+  if ( fileName.search("64x64_thumbnail") == -1 ){
+    // doesn't have a thumbnail, get the filename extension
+    var filename_split = fileName.split('.');
+    var filename_ext = filename_split[filename_split.length - 1];
+    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
+    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
+      // only support png and jpg at this point
+      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
+      const gcsObject = bucket.file(fileName);
+      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
+      let gcsNewObject = bucket.file(newFilename);
+      let srcStream = gcsObject.createReadStream();
+      let dstStream = gcsNewObject.createWriteStream();
+      let resize = imagemagick().resize(size).quality(90);
+      srcStream.pipe(resize).pipe(dstStream);
+      return new Promise((resolve, reject) => {
+        dstStream
+          .on("error", (err) => {
+            console.log(`Error: ${err}`);
+            reject(err);
+          })
+          .on("finish", () => {
+            console.log(`Success: ${fileName} → ${newFilename}`);
+              // set the content-type
+              gcsNewObject.setMetadata(
+              {
+                contentType: 'image/'+ filename_ext.toLowerCase()
+              }, function(err, apiResponse) {});
+              pubsub
+                .topic(topicName)
+                .publisher()
+                .publish(Buffer.from(newFilename))
+                .then(messageId => {
+                  console.log(`Message ${messageId} published.`);
+                })
+                .catch(err => {
+                  console.error('ERROR:', err);
+                });
+          });
+      });
+    }
+    else {
+      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
+    }
+  }
+  else {
+    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
+  }
+});
+EOF
+
+cat << EOF > package.json
+{
+  "name": "thumbnails",
+  "version": "1.0.0",
+  "description": "Create Thumbnail of uploaded image",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@google-cloud/functions-framework": "^3.0.0",
+    "@google-cloud/pubsub": "^2.0.0",
+    "@google-cloud/storage": "^5.0.0",
+    "fast-crc32c": "1.0.4",
+    "imagemagick-stream": "4.1.1"
+  },
+  "devDependencies": {},
+  "engines": {
+    "node": ">=4.3.2"
+  }
+}
+EOF
+
+#Disable and Re-enable the Cloud Functions API:
+gcloud services disable cloudfunctions.googleapis.com && gcloud services enable cloudfunctions.googleapis.com
+
+#Add permissions
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member="serviceAccount:$PROJECT_ID@appspot.gserviceaccount.com" \
+--role="roles/iam.serviceAccountTokenCreator"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
+
+gcloud functions deploy $FUNC \
+  --region=$REGION \
+  --gen2 \
+  --entry-point=$FUNC \
+  --trigger-bucket=$BCKT \
+  --runtime=nodejs20
+
+#Verify status
+gcloud functions describe $FUNC
+
+echo "4. Testing infra"
+#Upload a PNG or JPG image
+EXTERNAL_ASSET=https://storage.googleapis.com/cloud-training/gsp315/map.jpg
+
+FILE=$FUNC.jpg
+curl $EXTERNAL_ASSET --output $FILE
+gsutil cp $FILE gs://$BCKT
+
+echo "End of script. Go take that guy's access out"
